e6cd87ba93f733704cb5886222606a801c0b7f1a
Marc Alban <marcalban@hatchbed.com>
2024-11-20 15:08:24 -0600

    Fix caffe2 import error.

--- detectron2/export/__init__.py
+++ detectron2/export/__init__.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from .api import *
 from .flatten import TracingAdapter
 from .torchscript import scripting_with_instances, dump_torchscript_IR
+
+try:
+    from caffe2.proto import caffe2_pb2 as _tmp
+    from caffe2.python import core
+    from .api import *
+    # caffe2 is optional
+except ImportError:
+    pass
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
